f7464a15512628c784222980fdc5356b5f54c420
Valérian Galliat <val@codejam.info>
2015-09-10 18:18:24 +0200

    Externalize rendering logic to WebTorrent

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 var WebTorrent = require('webtorrent')
-var MediaSourceStream = require('mediasource')
-var path = require('path')
 
 function findTorrents () {
   return Array.prototype.slice.call(document.querySelectorAll('[data-torrent]'))
 }
 
-var client = new WebTorrent({ dht: false })
+var client = new WebTorrent()
 
 findTorrents().forEach(function (elem) {
   elem.src = ''
 
   client.add(elem.dataset.torrent, function (torrent) {
-    var file = torrent.files[0]
-    var extname = path.extname(file.name).toLowerCase()
-
-    file.createReadStream().pipe(new MediaSourceStream(elem, { extname: extname }))
+    torrent.files[0].renderTo(elem)
   })
 })
